95fa6ad2b77ee55c87a2712e2ce56dbbac20a833
samriz <samriz@users.noreply.github.com>
2019-11-12 17:52:40 -0600

    change sprite pixels per unit for block and changed its color. Also, added Block.cs which makes the block disappear when something collides into it.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision) //engine will call this when ball hits block
+    {
+        Destroy(gameObject); //destroy block when something collides with it
+        Debug.Log(collision.gameObject.name); 
+    }
+}
