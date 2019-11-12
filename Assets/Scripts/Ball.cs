8693bf4f752891e9d2d4adc1c9d8979bc6e0dad8
samriz <samriz@users.noreply.github.com>
2019-11-12 01:09:47 -0600

    Refactored code in Ball.cs and are able to launch the ball from the paddle to effectively start the game.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
-
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
     //state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +20,25 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LockBallToPaddle();
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
     }
 
     void LockBallToPaddle()
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector; //ball's position is the position of the paddle plus the offset
+    }
+
+    void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
+        }
     }
 }
