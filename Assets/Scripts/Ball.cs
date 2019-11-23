f7c820d4d544192a6c5de58d471064d9333ce5b4
samriz <samriz@users.noreply.github.com>
2019-11-23 14:58:50 -0600

    Updated to latest version of Unity (2019.2.13f1) and playing different ball sounds randomly from an AudioSource array.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,14 +7,20 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+
     //state
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+
+    //cached component references
+    AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position; //ball position minus paddle position
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -44,6 +50,10 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(hasStarted) GetComponent<AudioSource>().Play();
+        if(hasStarted) 
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0,ballSounds.Length)]; //choose random value from audio array
+            myAudioSource.PlayOneShot(clip); //play that random clip
+        }
     }
 }
