29933786961f2d59fc2a8eeae7ec87c45827432e
samriz <samriz@users.noreply.github.com>
2019-11-24 00:05:18 -0600

    added sound to camera that is triggered every time a block is broken. Created Level.cs which will handle counting blocks and keeping score.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
     void OnCollisionEnter2D(Collision2D collision) //engine will call this when ball hits block
     {
+        AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
         Destroy(gameObject); //destroy block when something collides with it
-        Debug.Log(collision.gameObject.name); 
+        //Debug.Log(collision.gameObject.name); 
     }
 }
