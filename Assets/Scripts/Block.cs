aca4961a4ba1e6948bcae7628d10111c74489e6c
samriz <samriz@users.noreply.github.com>
2019-11-24 13:52:49 -0600

    Successfully count the number of blocks at the start of the game.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+
+    //cached reference
+    Level level;
+
+    void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     void OnCollisionEnter2D(Collision2D collision) //engine will call this when ball hits block
     {
         AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    [SerializeField] int breakableBlocks; //serialized for debugging purposes
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class Level : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
     }
 }
